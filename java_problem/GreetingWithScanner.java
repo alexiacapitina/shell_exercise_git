524efbc017ab21b0280bde71149541061cbfd1e4
eelacpa <alexia.capitina@ericsson.com>
2018-08-21 11:33:47 -0400

    Adding java files for practice

---
+++ java_problem/GreetingWithScanner.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GreetingWithScanner {
+    
+    public static void main(String[] args) {
+    
+        Scanner stdin = new Scanner( System.in );
+   	String me; 
+        String usersName;      // The user's name, as entered by the user.
+        String upperCaseName;  // The user's name, converted to upper case letters.
+        
+        System.out.print("Please enter your name: ");
+        usersName = stdin.nextLine();
+ 
+       
+        if(usersName.equals("Alice") || usersName.equals("Bob") ){
+            upperCaseName = usersName.toUpperCase();
+
+            System.out.println("Hello, " + upperCaseName + ", nice to meet you!");
+       } else {
+            System.out.println("Sorry, name not valid! ");
+
+	//upperCaseName= usersName.toUpperCase();
+        
+        //System.out.println("Hello, " + upperCaseName + ", nice to meet you!");
+        }
+
+    }  // end main()
+ 
+}  // end class

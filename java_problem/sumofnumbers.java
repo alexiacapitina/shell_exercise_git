c56a0e9ca799f5d66b92aed464489170b473f5cb
eelacpa <alexia.capitina@ericsson.com>
2018-08-21 12:04:06 -0400

    modified files

--- java_problem/sumofnumbers.java
+++ java_problem/sumofnumbers.java
@@ -1,22 +1,13 @@
 import java.util.Scanner;
 
- 
-
 public class sumofnumbers
-
 {
-
     public static void main(String[] arg) {
-
-
         System.out.println("Enter a positive number");  
 
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
-
- 
-
 
         int sum = 0;
 
